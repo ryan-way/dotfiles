98a468f9239c34330c16db0f0adaf72784950c82
Ryan Way <ryan.keith.way@gmail.com>
2021-09-30 08:34:31 -0700

    removing blurring to improve speed

--- .config/i3/lock.sh
+++ .config/i3/lock.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 SCREEN=/tmp/screen.png
 scrot $SCREEN
-convert $SCREEN -scale 10% -scale 1000% -blur 5x8 $SCREEN
+convert $SCREEN -scale 10% -scale 1000% $SCREEN
 [[ -f $1 ]] && convert $SCREEN $1 -gravity center -composite -matte $SCREEN
 dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Stop
 i3lock -u -i $SCREEN
