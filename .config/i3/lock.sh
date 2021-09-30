9770c2ae12e9a75a66debd083c3e82e328446750
Ryan Way <ryan.keith.way@gmail.com>
2021-09-30 15:25:01 -0700

    simplified i3 lock script

--- .config/i3/lock.sh
+++ .config/i3/lock.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-SCREEN=/tmp/screen.png
-scrot $SCREEN
-convert $SCREEN -scale 10% -scale 1000% $SCREEN
-[[ -f $1 ]] && convert $SCREEN $1 -gravity center -composite -matte $SCREEN
-dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Stop
-i3lock -u -i $SCREEN
-rm $SCREEN
+#SCREEN=/tmp/screen.png
+#scrot $SCREEN
+#convert $SCREEN -scale 10% -scale 1000% $SCREEN
+#[[ -f $1 ]] && convert $SCREEN $1 -gravity center -composite -matte $SCREEN
+#dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Stop
+#i3lock -u -i $SCREEN
+#rm $SCREEN
+i3lock -c 333333
