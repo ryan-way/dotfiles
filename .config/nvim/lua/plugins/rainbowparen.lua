a9b2462a22b9a18384eeec5b0455409a61b505af
Ryan Way <ryan.keith.way@gmail.com>
2021-11-08 19:47:36 -0800

    move lua files to lua subdirectory

--- .config/nvim/lua/plugins/rainbowparen.lua
+++ .config/nvim/lua/plugins/rainbowparen.lua
@@ -1,7 +1,7 @@
 vim.g.rbpt_max = 16
 vim.g.rbpt_loadcmd_toggle = 0
 
-vim.api.nvim_command([[
+vim.cmd([[
 au VimEnter * RainbowParenthesesToggle
 au Syntax * RainbowParenthesesLoadRound
 au Syntax * RainbowParenthesesLoadSquare
