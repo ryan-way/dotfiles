1fad5a1e43873ec7e0fe601028c8ea199ce73290
Ryan Way <ryan.keith.way@gmail.com>
2021-11-08 21:43:36 -0800

    fixed telescope remaps

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -4,13 +4,10 @@ require('telescope').setup({
       layout_config = {
         prompt_position='top'
       },
-      mappings = {
-        n = {
-          ["<C-p>"] = "git_files",
-          ["<leader>lr"] = "lsp_references",
-          ["<leader>li"] = "lsp_implementations",
-          ["<leader>ld"] = "lsp_definitions"
-        }
-      }
     },
   })
+local map = vim.api.nvim_set_keymap
+map('n', '<c-p>', [[<cmd>lua require('telescope.builtin').git_files()<CR>]], { noremap = true })
+map('n', '<leader>lr', [[<cmd>lua require('telescope.builtin').lsp_references()<CR>]], { noremap = true })
+map('n', '<leader>li', [[<cmd>lua require('telescope.builtin').lsp_implementations()<CR>]], { noremap = true })
+map('n', '<leader>ld', [[<cmd>lua require('telescope.builtin').lsp_definitions()<CR>]], { noremap = true })
