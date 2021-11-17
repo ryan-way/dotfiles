21286ed63463d104684972a88d86ecc438e277dc
ryan-way <ryan.keith.way@gmail.com>
2021-11-17 08:05:20 -0800

    hide dotfiles by default in netrw

--- .config/nvim/lua/netrw.lua
+++ .config/nvim/lua/netrw.lua
@@ -2,6 +2,7 @@ vim.g.netrw_altv = 1
 vim.g.netrw_banner = 0
 vim.g.netrw_liststyle = 3
 vim.g.netrw_preview = 1
+vim.g.netrw_list_hide = "^\\..*"
 
 local map = vim.api.nvim_set_keymap
 map ( 'n', '<leader>e', ':Explore<cr>', { noremap = true })
