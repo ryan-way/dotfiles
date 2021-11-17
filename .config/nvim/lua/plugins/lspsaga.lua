d6064e8bf4c6f54567fda723c7c2d8192f38fe83
ryan-way <ryan.keith.way@gmail.com>
2021-11-17 07:58:32 -0800

    fix spelling error

--- .config/nvim/lua/plugins/lspsaga.lua
+++ .config/nvim/lua/plugins/lspsaga.lua
@@ -5,5 +5,5 @@ map('n', '<leader>ca', [[<cmd>lua require('lspsaga.codeaction').code_action()<CR
 map('n', 'K', [[<cmd>lua require('lspsaga.hover').render_hover_doc()<CR>]], {noremap = true} )
 map('n', 'gs', [[<cmd>lua require('lspsaga.signaturehelp').signature_help()<CR>]], {noremap = true, silent = true} )
 map('n', 'pd', [[<cmd>lua require('lspsaga.provider').preview_definition()<CR>]], {noremap = true, silent = true} )
-map('n', '[e', [[<cmd>lua require('lspsage.diagnostics').lsp_jump_diagnostics_prev()<CR>]], {noremap = true, silent = true} )
-map('n', ']e', [[<cmd>lua require('lspsage.diagnostics').lsp_jump_diagnostics_next()<CR>]], {noremap = true, silent = true} )
+map('n', '[e', [[<cmd>lua require('lspsaga.diagnostics').lsp_jump_diagnostics_prev()<CR>]], {noremap = true, silent = true} )
+map('n', ']e', [[<cmd>lua require('lspsaga.diagnostics').lsp_jump_diagnostics_next()<CR>]], {noremap = true, silent = true} )
