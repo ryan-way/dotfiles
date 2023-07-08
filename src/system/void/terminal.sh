ac0bd9a2d39d47f26403a6a43584f7d879b11ebd
ryan-way <ryan.keith.way@gmail.com>
2023-07-07 22:05:43 -0700

    Added aws cli to install script

--- src/system/void/terminal.sh
+++ src/system/void/terminal.sh
@@ -21,7 +21,8 @@ sudo xbps-install -y\
   starship \
   neovim \
   glow \
-  neofetch
+  neofetch \
+  aws-cli
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 chsh -s $(which zsh)
 LV_BRANCH='release-1.3/neovim-0.9' zsh <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh)
